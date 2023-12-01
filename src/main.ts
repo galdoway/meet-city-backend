a3c327267a8d20bc47ebbb6efb0951e5a6b1c137
Galdámez <me@galdoway.dev>
2023-12-01 11:03:02 -0600

    feat: ws adapter added

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { SwaggerModule } from '@nestjs/swagger';
-import { swaggerConfig } from './configs';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { WsAdapter } from '@nestjs/platform-ws';
+import { swaggerConfig } from './configs';
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule);
+	app.useWebSocketAdapter(new WsAdapter(app));
 	app.enableVersioning({
 		type: VersioningType.URI,
 		defaultVersion: '1',
